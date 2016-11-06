b2b20ddc5a8c5f7e545e94ed1426b4271c086df4
ROMY\Romy96 <romybijkerk1996@gmail.com>
2016-11-06 13:19:19 +0100

    Login

--- Login.php
+++ Login.php
@@ -5,13 +5,5 @@
  require_once 'inc/user_helpers.php';
 
 
-if ( IsLoggedInSession()==true ) {
-	// stuur direct door naar main pagina
-	header('location: Adressenlijst.php');
-	exit();
-}
-else
-{
 // output everything
 echo $blade->view()->make('Backend/Login')->withErrors($errors)->render();
-}
